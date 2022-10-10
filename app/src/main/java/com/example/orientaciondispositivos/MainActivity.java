eaf95a3d94226dac29984dc4d8b6c5a3ce8783fc
Cesar Flores <92449662+CesarFloresH@users.noreply.github.com>
2022-10-10 11:54:03 -0600

    Implementación y configuración de los componentes. (#1) (#2)
    * Se implemento en el archivo activity_main.xml los componentes que se utilizara.
    * Se implemento la configuración del MainActivity.
    * Se agrego la configuración de los componentes que se agrego de activity_main.

--- app/src/main/java/com/example/orientaciondispositivos/MainActivity.java
+++ app/src/main/java/com/example/orientaciondispositivos/MainActivity.java
@@ -1,13 +1,93 @@
 package com.example.orientaciondispositivos;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private long last_update = 0, last_movement = 0;
+    private float prevX = 0, prevY = 0, prevZ = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
     }
+
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        synchronized (this) {
+            long current_time = event.timestamp;
+            float curX = event.values[0];
+            float curY = event.values[1];
+            float curZ = event.values[2];
+            if (prevX == 0 && prevY == 0 && prevZ == 0) {
+                last_update = current_time;
+                last_movement = current_time;
+                prevX = curX;
+                prevY = curY;
+
+                prevZ = curZ;
+            }
+
+            long time_difference = current_time - last_update;
+            if (time_difference > 0) {
+                float movement = Math.abs((curX + curY + curZ) - (prevX - prevY -
+                        prevZ)) / time_difference;
+                int limit = 1500;
+                float min_movement = 1E-6f;
+                if (movement > min_movement) {
+                    if (current_time - last_movement >= limit) {
+                        Toast.makeText(getApplicationContext(), "Hay movimiento de "
+                                + movement, Toast.LENGTH_SHORT).show();
+                    }
+                    last_movement = current_time;
+                }
+                prevX = curX;
+                prevY = curY;
+                prevZ = curZ;
+                last_update = current_time;
+            }
+            ((TextView) findViewById(R.id.txtAccX)).setText("Acelerómetro X: " +
+                    curX);
+            ((TextView) findViewById(R.id.txtAccY)).setText("Acelerómetro Y: " +
+                    curY);
+            ((TextView) findViewById(R.id.txtAccZ)).setText("Acelerómetro Z: " +
+                    curZ);
+        }}
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        List<Sensor> sensors = sm.getSensorList(Sensor.TYPE_ACCELEROMETER);
+        if (sensors.size() > 0) {
+            sm.registerListener(this, sensors.get(0),
+                    SensorManager.SENSOR_DELAY_GAME);
+        }}
+
+    protected void onStop() {
+        SensorManager sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sm.unregisterListener(this);
+        super.onStop();
+    }
+
 }
